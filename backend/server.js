da46823809a6a2bbce20508efa680fb2b686f109
jakareya76 <jakareya1306@gmail.com>
2024-09-14 14:20:45 +0600

    init

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/products", (req, res) => {
+  res.json({ message: "hello world" });
+});
+
+app.listen(5000, () => {
+  console.log("Server runing on port 5000");
+});
