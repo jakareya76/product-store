a777f621afff9ad598a7cd65868ff6097bfd728c
jakareya76 <jakareya1306@gmail.com>
2024-09-16 20:56:41 +0600

    create product post api

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-  res.json({ message: "hello world" });
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error(`Error in create product ${error}`);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 });
 
 app.listen(5000, () => {
